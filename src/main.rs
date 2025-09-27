0b16d4cbec699ceba8f22a539b267c3808bac0ba
Evgenii Troinov <troinof@gmail.com>
2025-09-27 23:21:12 +0200

    master: Refactor analyze option

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,6 @@
-use bytesize::ByteSize;
+mod analyzer;
+
 use clap::Parser;
-use comfy_table::Table;
-use fs_extra::dir::get_size;
-use glob::{glob_with, MatchOptions};
-use std::collections::HashMap;
-
-const PATTERNS: [&str; 6] = ["tsx", "ts", "md", "html", "map", "test.*"];
-
-fn analyze(path: &str) {
-    let mut table = Table::new();
-    let mut data: HashMap<String, [u64; 2]> = HashMap::new();
-
-    for pattern in PATTERNS {
-        let mut total_size: u64 = 0;
-        let mut total_count: u64 = 0;
-
-        if let Ok(files) = glob_with(
-            &format!("{}/**/*.{}", path, pattern),
-            MatchOptions {
-                case_sensitive: false,
-                require_literal_separator: true,
-                require_literal_leading_dot: true,
-            },
-        ) {
-            for file in files {
-                if let Ok(size) = get_size(file.unwrap()) {
-                    total_size += size;
-                    total_count += 1;
-                }
-            }
-
-            data.insert(pattern.to_string(), [total_size, total_count]);
-        }
-    }
-
-    table.set_header(vec!["Pattern", "Files count", "Total size"]);
-
-    let mut posible_space_to_free: u64 = 0;
-
-    for (key, value) in &data {
-        posible_space_to_free += value[0];
-        table.add_row(vec![
-            format!("*.{}", key),
-            value[1].to_string(),
-            ByteSize(value[0]).to_string(),
-        ]);
-    }
-
-    println!("{table}");
-    println!(
-        "we can safely reduce the size of the node_modules folder by {}",
-        ByteSize(posible_space_to_free).to_string()
-    )
-}
 
 #[derive(Parser, Debug)]
 #[command(name = "my_utility")]
@@ -73,7 +21,7 @@ fn main() {
     let path = args.path.as_deref().unwrap_or(ROOT_DIR_PATH);
 
     if args.analyze {
-        analyze(&path);
+        analyzer::run(&path);
     }
 
     println!("{:?}", args);
